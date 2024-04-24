2c3939f7e024e65aeca9929401a3474f1e9a087e
Ryan Reeves <ryan.anthony.reeves@gmail.com>
2024-04-24 18:51:30 -0400

    Updated JS script file with function outlines

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const {MongoClient} = require('mongodb')
 
+
+//Connect to our database
+async function main() {
+
+}
+
+
+function validate(){
+    //Retrieve Username and password from form and store them
+    //Compare with username and password in database
+    //If both match, redirect to dashboard or profile
+}
+
+function createUser(){
+    //retrieve information from form
+    //Parse into JSON format
+    //Store in database
+}
