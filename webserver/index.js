bc55215cb2b4380fb06686967b17edba6d911ba2
Corster8 <caeahaiteah@gmail.com>
2024-04-29 20:04:46 -0400

    removed unused package body-parser

--- webserver/index.js
+++ webserver/index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const bodyParser = require('body-parser');
 const path = require("path");
 const cookieParser = require('cookie-parser');
 
